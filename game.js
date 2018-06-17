d54ba186c8a01bc69815f59507ad111024229152
scott88lee <scott88lee@gmail.com>
2018-06-17 14:25:12 +0800

    wrote basic functions

--- game.js
+++ game.js
@@ -1,17 +1,26 @@
-function generateBoard(rows, cols) {  //Dynamically generate board
-  for (var i=0; i<rows; i++) {
-    for (var k=0; k<cols; k++) {
+var gameChar = ['@', 'X', 'A', 'M'];
+var shadowBoard = [];
+
+function generateBoard(size) {  //Dynamically generate board
+  for (var i=0; i<size; i++) {
+    
+    shadowBoard.push([]);
+
+    for (var k=0; k<size; k++) {
 
       //Box factory
       var box = document.createElement('div');
       
       box.row = i;
       box.col = k;
-
+      box.gameValue = gameChar[Math.floor(Math.random() * 4)];
+      shadowBoard[i].push(box.gameValue);
+      
       box.style.display = "inline-block";
       box.style.border = "2px solid black";
       box.style.height = "100px";
       box.style.width = "100px";
+      box.textContent = box.gameValue;
       document.body.appendChild(box);
 
       //Adds listeners to every button
@@ -23,6 +32,25 @@ function generateBoard(rows, cols) {  //Dynamically generate board
   }
 }
 
+function updateDisplay(){
+  var divList = document.querySelectorAll('div');
+  for (var i=0; i<shadowBoard.length; i++){
+    for (var k=0; k<shadowBoard[i].length; k++){
+      divList[(i*5)+k].textContent = shadowBoard[i][k];
+    }
+  }
+}
+
+function randomizeShadowBoard(){
+  for (var i=0; i<shadowBoard.length; i++){
+    for (var k=0; k<shadowBoard[i].length; k++){
+      shadowBoard[i][k] = gameChar[Math.floor(Math.random() * 4)];
+    }
+  }
+}
+
 function game() {
   console.log('Hey');
 }
+
+generateBoard(5, 5); 
