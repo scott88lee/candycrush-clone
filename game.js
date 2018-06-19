7bb71c2127e6a6d24bf15e792a7e6627603cfdb7
scott88lee <scott88lee@gmail.com>
2018-06-19 08:24:33 +0800

    started on game logic

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var move = [];
 function generateBoard(size) {  //Dynamically generate board
   for (var i=0; i<size; i++) {
     
-    shadowBoard.push([]);
+    shadowBoard.push([]);  // [[A,@,X,M,A],[]]
 
     for (var k=0; k<size; k++) {
 
@@ -33,6 +33,7 @@ function generateBoard(size) {  //Dynamically generate board
     document.body.appendChild(br);
     //Creates break for the next row
   }
+  game();
 }
 
 function updateDisplay(){
@@ -44,16 +45,50 @@ function updateDisplay(){
   }
 }
 
-function randomizeShadowBoard(){
+function randomizeBoard(){
   for (var i=0; i<shadowBoard.length; i++){
     for (var k=0; k<shadowBoard[i].length; k++){
-      shadowBoard[i][k] = gameChar[Math.floor(Math.random() * 4)];
+      //if (shadowBoard[i][k] == ' '){
+        shadowBoard[i][k] = gameChar[Math.floor(Math.random() * 4)];
+      //}
+    }
+  }
+}
+
+function fillHoles(){
+  for (var i=0; i<shadowBoard.length; i++){
+    for (var k=0; k<shadowBoard[i].length; k++){
+      if (shadowBoard[i][k] == ' '){
+        shadowBoard[i][k] = gameChar[Math.floor(Math.random() * 4)];
+        vMatch = [];
+        hMatch = [];
+      }
     }
   }
 }
 
 function game() {
- console.log('Hey');
+  if (move.length < 2){
+    move.push([this.row,this.col]);
+    console.log(move);
+  } else if (move.length == 2){
+    console.log(move);
+    var tempX = shadowBoard[move[0][0]][move[0][0]];
+    var tempY = shadowBoard[move[0][0]][move[0][1]];
+    shadowBoard[move[0][0]][move[0][0]] = shadowBoard[move[1][0]][move[1][0]];
+    shadowBoard[move[0][0]][move[0][1]] = shadowBoard[move[1][0]][move[1][1]];
+    shadowBoard[move[1][0]][move[1][0]] = tempX;
+    shadowBoard[move[1][0]][move[1][1]] = tempY;
+    move = [];
+  }
+
+  do {
+    checkBoard();
+    clearMatches();
+    updateDisplay();
+    fillHoles();
+    setTimeout(updateDisplay, 400);
+  } while ( checkBoard() );
 }
 
 function checkBoard(){
@@ -62,6 +97,7 @@ function checkBoard(){
     for (var k=0; k<shadowBoard.length-2; k++){
       if (shadowBoard[i][k] == shadowBoard[i][k+1] && shadowBoard[i][k+1] == shadowBoard[i][k+2]){
         hMatch.push([i,k]);
+        return true;
       }
     }
   }
@@ -70,11 +106,24 @@ function checkBoard(){
     for (var k=0; k<shadowBoard.length; k++){
       if (shadowBoard[i][k] == shadowBoard[i+1][k] && shadowBoard[i+1][k] == shadowBoard[i+2][k]){
         vMatch.push([i,k]);
+        return true;
       }
     }
   }
 }  
 
+function clearMatches() {
+  for (var i=0; i<hMatch.length; i++){
+    shadowBoard[hMatch[i][0]][hMatch[i][1]] = ' ';
+    shadowBoard[hMatch[i][0]][hMatch[i][1]+1] = ' ';
+    shadowBoard[hMatch[i][0]][hMatch[i][1]+2] = ' ';
+  }
+  for (var i=0; i<vMatch.length; i++){
+    shadowBoard[vMatch[i][0]][vMatch[i][1]] = ' ';
+    shadowBoard[vMatch[i][0]+1][vMatch[i][1]] = ' ';
+    shadowBoard[vMatch[i][0]+2][vMatch[i][1]] = ' ';
+  }
+}
 
 generateBoard(5); 
 
